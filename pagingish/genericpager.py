c0faea200ac14c8c382463ede381f4c6e0e182a4
Matt Goodall <matt.goodall@gmail.com>
2009-04-01 18:34:38 +0100

    Add a generic pager that calls whatever is passed with 'skip' and 'limit' args.

---
+++ pagingish/genericpager.py
@@ -0,0 +1,33 @@
+class SkipLimitPager(object):
+    """
+    Generic efficient pager that simply calls a function, passing it item skip
+    and limit.
+
+    The func that is passed to the initialiser must have a signature of "def
+    func(skip=None, limit=None)".
+
+    Efficient simply means that the pager does not try to calculate any paging
+    statistics, i.e. there is no need to calculated total items, total pages,
+    etc.
+    """
+
+    def __init__(self, func):
+        self.func = func
+
+    def get(self, pagesize, pageref=None):
+        # Decode the pageref.
+        if pageref is not None:
+            pagenum = int(pageref)
+        else:
+            pagenum = 0
+        # Get a page of items with one extra to test if there's a next page.
+        items = self.func(skip=pagenum*pagesize, limit=pagesize+1)
+        # Calculate prev and next refs
+        prev, next = None, None
+        if pagenum:
+            prev = unicode(pagenum-1)
+        if len(items) > pagesize:
+            next = unicode(pagenum+1)
+        # Return result tuple.
+        return prev, items[:pagesize], next, None
+
---
+++ pagingish/tests/test_generic.py
@@ -0,0 +1,39 @@
+import unittest
+
+from pagingish.genericpager import SkipLimitPager
+
+
+def pager_func(l):
+    def func(skip=None, limit=None):
+        if skip is None:
+            skip = 0
+        if limit is None:
+            limit = len(l)
+        return l[skip:skip+limit]
+    return func
+
+
+class TestSkipLimitPager(unittest.TestCase):
+
+    def test_walk(self):
+        pager = SkipLimitPager(pager_func(range(20)))
+        prev, items, next, stats = pager.get(5)
+        assert items == range(0, 5)
+        assert prev is None
+        assert next
+        prev, items, next, stats = pager.get(5, next)
+        assert items == range(5, 10)
+        assert prev
+        assert next
+        prev, items, next, stats = pager.get(5, next)
+        prev, items, next, stats = pager.get(5, next)
+        assert items == range(15, 20)
+        assert prev
+        assert not next
+        prev, items, next, stats = pager.get(5, prev)
+        prev, items, next, stats = pager.get(5, prev)
+        prev, items, next, stats = pager.get(5, prev)
+        assert items == range(0, 5)
+        assert prev is None
+        assert next
+
