e9c5b4b6fe9060af973ee2d4f88a17fd2b3ad279
Matt Goodall <matt.goodall@gmail.com>
2009-05-01 14:58:05 +0100

    Serialize the pageref to and from a string.
    The (undocumented) idea of a pageref was for it to be an opaque value
    only understood by the pager implementation. The ListPager assumed it
    was an int already, meaning something else outside the ListPager had to
    know to turn it into an int and therefore what serialization the
    ListPager uses.

--- pagingish/list_pager.py
+++ pagingish/list_pager.py
@@ -5,9 +5,11 @@ class ListPager(object):
 
     def get(self, pagesize, pageref=None):
 
-        # Decode the pageref
+        # Decode the pageref, defaulting to page 1
         if pageref is None:
             pageref = 1
+        else:
+            pageref = int(pageref)
 
         # Get some stats
         item_count = len(self.data)
@@ -17,12 +19,12 @@ class ListPager(object):
         if (pageref-1) < 1:
             prevref = None
         else:
-            prevref = pageref-1
+            prevref = unicode(pageref-1)
 
         if (pageref+1) > total_pages:
             nextref = None
         else:
-            nextref = pageref+1
+            nextref = unicode(pageref+1)
 
         # Get stats
         stats = {'item_count': item_count, 'total_pages': total_pages}
