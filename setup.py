2c57c6472036e499274b63aecafb488f64eaf8dc
Tim Parkin <tim.parkin@jdi-associates.com>
2010-03-11 16:24:14 +0000

    added names and description to setup

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '0.1'
 
 setup(name='pagingish',
       version=version,
-      description="",
+      description=" Generic paging library - includes couchdb paging",
       long_description="""\
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
-      author='',
-      author_email='',
-      url='',
+      author='Tim Parkin & Matt Goodall',
+      author_email='info@timparkin.co.uk',
+      url='http://ish.io',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
